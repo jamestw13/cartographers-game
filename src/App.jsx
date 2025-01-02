75e25f3ed89e54e863690b8e50eec029ab051d00
TJ James <jamestw13@gmail.com>
2025-01-01 22:21:05 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import './App.css';
+
+import { edictCards, scoringCards, seasonCards } from './cards';
+
+function App() {
+  const NUM_EDICTS = 4;
+  const [seasonIndex, setSeasonIndex] = useState(0);
+  const [edict, setEdict] = useState([]);
+
+  const handleSetEdicts = () => {
+    const newEdicts = [];
+    for (let i = 0; i < 4; i++) {
+      const index = Math.floor(Math.random() * scoringCards.length);
+      const newEdict = scoringCards.splice(index, 1);
+      newEdicts.push(newEdict[0]);
+
+      // setEdict(x => x.append(edict[0]));
+    }
+    console.log(newEdicts);
+    setEdict(newEdicts);
+  };
+  return (
+    <>
+      <button onClick={handleSetEdicts}>Start Game</button>
+      <div className="season-edict-container">
+        <div className="season-container">{seasonCards[seasonIndex]}</div>
+
+        <div className="edict-a-container">
+          <div>{edictCards[0]}</div>
+          <div>{edict.length > 0 ? edict[0].name : 'waiting'} </div>
+        </div>
+
+        <div className="edict-b-container">
+          <div>B</div>
+          <div>{edict.length > 0 ? edict[1].name : 'waiting'} </div>
+        </div>
+        <div className="edict-c-container">
+          <div>C</div>
+          <div>{edict.length > 0 ? edict[2].name : 'waiting'} </div>
+        </div>
+        <div className="edict-d-container">
+          <div>D</div>
+          <div>{edict.length > 0 ? edict[3].name : 'waiting'} </div>
+        </div>
+        {seasonIndex !== 3 ? (
+          <button
+            onClick={() => {
+              setSeasonIndex(x => x + 1);
+            }}
+          >
+            Next Season
+          </button>
+        ) : (
+          <div>Done</div>
+        )}
+      </div>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/cards.js
@@ -0,0 +1,22 @@
+export const scoringCards = [
+  { name: 'Faunlost Thicket' },
+  { name: 'Heart of the Forest' },
+  { name: 'Sleepy Valley' },
+  { name: 'Deepwood' },
+  { name: 'Gnomish Colony' },
+  { name: 'Outer Enclave' },
+  { name: 'Caravansary' },
+  { name: 'Traylo Monastery' },
+  { name: 'Banded Hills' },
+  { name: 'Starlit Sigil' },
+  { name: 'Silos' },
+  { name: 'Dwarvenholds' },
+  { name: "Ulem's Wallow" },
+  { name: 'Jorekburg' },
+  { name: 'Craylund' },
+  { name: 'Clawsgrave Peaks' },
+];
+
+export const seasonCards = ['Spring', 'Summer', 'Autumn', 'Winter'];
+
+export const edictCards = ['A', 'B', 'C', 'D'];
